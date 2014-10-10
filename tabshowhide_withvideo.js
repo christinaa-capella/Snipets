688cec03485dd8e94ff0ddd7fc45e9e7ce88c7e3
christinaa-capella <christina.adams@capella.edu>
2014-10-10 09:12:00 -0500

    Update tabshowhide_withvideo.js
    documented the function and added some comments for better explanation.

--- tabshowhide_withvideo.js
+++ tabshowhide_withvideo.js
@@ -3,17 +3,17 @@
 //this function animates opacity and moves the position off screen
 
 $(nav).on('click', 'ul li a', function(e){
-		e.preventDefault();
-		// pauseAllMedia(); XXUncoment for live
-		var mycontent = $(this).attr('href');
-		//Hides all tabcontent using opacity and moving off screen 
-		//(due to video player error cannot show/hide/fadein/fadeout)
-		tabContent.css({'position':'absolute', 'left' : '-999em', 'opacity' : '0'});
-		nav.find('.activetab').removeClass('activetab');
-		$(this).addClass('activetab');
-		//Shows mycontent by using opacity and bringing onscreen 
-		//(due to video player error cannot show/hide/fadein/fadeout)
-		$(mycontent).css({ 'position' : 'relative', 'left': '0px'}).animate({'opacity' : 1}, 100);
-	});
-
-	$(nav).find('ul li:first-child a').trigger("click");
+	e.preventDefault();
+	// pauseAllMedia(); XXUncoment for live
+	var mycontent = $(this).attr('href');
+	//Hides all tabcontent using opacity and moving off screen 
+	//(due to video player error cannot show/hide/fadein/fadeout)
+	tabContent.css({'position':'absolute', 'left' : '-999em', 'opacity' : '0'});
+	nav.find('.activetab').removeClass('activetab');
+	$(this).addClass('activetab');
+	//Shows mycontent by using opacity and bringing onscreen 
+	//(due to video player error cannot show/hide/fadein/fadeout)
+	$(mycontent).css({ 'position' : 'relative', 'left': '0px'}).animate({'opacity' : 1}, 100);
+});
+//This finds the first tab on page load and forces a click to make it visible.
+$(nav).find('ul li:first-child a').trigger("click");
